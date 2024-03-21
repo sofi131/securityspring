68a15442ddda967275d2c825cb7b30a37affe343
sofi131 <sofiasanmartin131@gmail.com>
2024-03-21 16:35:14 +0100

    application.properties

--- src/test/java/com/ceica/securityspring/SecurityspringApplicationTests.java
+++ src/test/java/com/ceica/securityspring/SecurityspringApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SecurityspringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
